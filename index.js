634c8dfcc476e66bb5cf222f67eb3e3d3f5693ce
Your Name <leticiagrandprime@gmail.com>
2023-08-19 20:22:21 -0300

    versao inicial

---
+++ index.js
@@ -0,0 +1,21 @@
+var setaEsquerda = window.document.getElementById("seta-esquerda")
+var leonardo = window.document.getElementById("leonardo")
+var samantha = window.document.getElementById("samantha")
+var bruna = window.document.getElementById("bruna")
+var setaDireita = window.document.getElementById("seta-direita")
+
+
+function RolarParaDireita(){
+    leonardo.style ="display: none"
+    bruna.style ="display: flex"
+    setaDireita.style = "display: none"
+    setaEsquerda.style = "display: flex; margin-top: 55px"
+}
+
+function RolarParaEsquerda(){
+    leonardo.style ="display: flex"
+    bruna.style ="display: none"
+    setaDireita.style = "display: flex"
+    setaEsquerda.style = "display: none"
+}
+
