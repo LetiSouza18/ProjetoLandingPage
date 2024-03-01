b2363bf36761d06bea114cfddd8e296cc106c9a9
LetiSouza18 <leticiagrandprime@gmail.com>
2024-03-01 17:15:55 -0300

    Alinhamento

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function RolarParaDireita(){
     leonardo.style ="display: none"
     bruna.style ="display: flex"
     setaDireita.style = "display: none"
-    setaEsquerda.style = "display: flex; margin-top: 50px"
+    setaEsquerda.style = "display: flex; margin-top: 55px"
 }
 
 function RolarParaEsquerda(){
     leonardo.style ="display: flex"
     bruna.style ="display: none"
-    setaDireita.style = "display: flex"
+    setaDireita.style = "display: flex; margin-top: 50px"
     setaEsquerda.style = "display: none"
 }
 
